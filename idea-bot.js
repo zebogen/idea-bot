571e5ea71838133a1beae1bee47326dfd2fbc8fd
Matt Bogen <zebogen@gmail.com>
2016-08-22 21:19:02 -0500

    Fix bug in reply

--- idea-bot.js
+++ idea-bot.js
@@ -125,6 +125,7 @@ controller.hears(['^#([^ ]*) <([^ ]*)>'], 'direct_message,direct_mention,mention
 
 controller.hears(['.*'], 'direct_message,direct_mention,mention', function(bot, message) {
   bot.reply(
+    message,
     "I'm sorry, I didn't recognize that command. To add to your idea lists, send me a direct message " +
     "or mention @idea-bot followed by a hashtag and the URL you'd like to save. " +
     "Supported hashtags: #product, #article, #body, #mind, #lifestyle." +
